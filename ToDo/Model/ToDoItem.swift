3934df31dab6ba5fc9ba545b75d7dc2b0186c1a3
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-19 16:02:16 +0700

    ToDoItem has title

---
+++ ToDo/Model/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ToDo
+//
+//  Created by Supakit Thanadittagorn on 8/19/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    let title: String
+}
--- ToDoTests/Model/ToDoItemTests.swift
+++ ToDoTests/Model/ToDoItemTests.swift
@@ -1,15 +1,16 @@
 //
-//  ToDoTests.swift
-//  ToDoTests
+//  ToDoItemTests.swift
+//  ToDo
 //
 //  Created by Supakit Thanadittagorn on 8/19/2559 BE.
 //  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
 //
 
 import XCTest
+
 @testable import ToDo
 
-class ToDoTests: XCTestCase {
+class ToDoItemTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -20,17 +21,9 @@ class ToDoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testInit_ShouldTakeTitle(){
+        _ = ToDoItem(title : "Title")
     }
     
 }
