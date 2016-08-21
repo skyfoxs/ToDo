4b35e26ce8fac5cb972f2f760705e9d65ea41561
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-21 21:47:02 +0700

    Refactor format indent

--- ToDo/Model/ItemManager.swift
+++ ToDo/Model/ItemManager.swift
@@ -9,26 +9,24 @@
 import Foundation
 
 class ItemManager {
+
     private var toDoItems = [ToDoItem]()
-    var toDoCount: Int {
-        return toDoItems.count
-    }
     private var doneItems = [ToDoItem]()
-    var doneCount: Int {
-        return doneItems.count
-    }
+
+    var toDoCount: Int { return toDoItems.count }
+    var doneCount: Int { return doneItems.count }
 
     func addItem(item: ToDoItem) {
         toDoItems.append(item)
     }
 
-    func itemAtIndex(index: Int) -> ToDoItem {
-        return toDoItems[index]
-    }
-
     func checkItemAtIndex(index: Int) {
         let item = toDoItems.removeAtIndex(index)
         doneItems.append(item)
+    }
+
+    func itemAtIndex(index: Int) -> ToDoItem {
+        return toDoItems[index]
     }
 
     func doneItemAtIndex(index: Int) -> ToDoItem {
